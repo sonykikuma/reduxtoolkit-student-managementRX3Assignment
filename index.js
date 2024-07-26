eb63cf7f3386289bd2b844a0e196c2499d791045
Abha Soni <103509382+sonykikuma@users.noreply.github.com>
2024-07-26 04:54:02 +0000

    port changes

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ app.delete("/students/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+//process.env.PORT ||
